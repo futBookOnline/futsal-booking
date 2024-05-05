a688d8a9a4657d104906643237fa10ac383b3138
BishalAryal1067 <celestial@pop-os.localdomain>
2024-05-05 20:22:14 +0545

    FMA-14/View page UI/ Router Configuration

---
+++ src/modules/futsal/pages/Futsal.jsx
@@ -0,0 +1,12 @@
+import GlobalLayout from "@/layouts/Global/GlobalLayout";
+const Futsal = () => {
+  return (
+    <GlobalLayout>
+      <div>
+        This is futsal page
+      </div>
+    </GlobalLayout>
+  );
+};
+
+export default Futsal;
--- src/modules/home/components/FutsalCard.jsx
+++ src/modules/home/components/FutsalCard.jsx
@@ -1,7 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 const FutsalCard = ({ futsalObj }) => {
   let { name, location, price } = futsalObj;
+  const navigate = useNavigate();
+
+  {
+    /*function to handle the click of each card*/
+  }
+  const handleClick = (name) => {
+    navigate(`/futsals/${name.toLowerCase()}`);
+  };
+
   return (
-    <div className="futsal-card">
+    <div className="futsal-card" onClick={handleClick(name)}>
       <h3>{name}</h3>
       <p>{location}</p>
       <p>Rs. {price} per hour</p>
